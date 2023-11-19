f3cd2b91901023af69833b0d29a2476139dbe030
Arne Maier <Arne_Maier@gmx.de>
2023-11-19 22:04:50 +0100

    Write and read base url from window

---
+++ frontend/docker/entrypoint.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+echo $URL
+sed -i "s/.*BASE_URL.*/window.BASE_URL=$(echo $URL | sed -En 's/\//\\\//pg');/g" /var/www/index.html
+
+nginx -g "daemon off;"
--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,1 @@
-export const BASE_URL = "http://localhost:8080/api";
-//export const BASE_URL = "https://192.168.178.94:8080/api";
+export const BASE_URL = (window as any).BASE_URL || "http://localhost:8080/api";
