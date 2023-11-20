66de60a7fa196f6ccdf8d756d90a20d114951371
Arne Maier <Arne_Maier@gmx.de>
2023-11-20 21:24:54 +0100

    Fix BASE_URL

--- frontend/docker/entrypoint.sh
+++ frontend/docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 echo $URL
-sed -i "s/.*BASE_URL.*/window.BASE_URL=$(echo $URL | sed -En 's/\//\\\//pg');/g" /var/www/index.html
+sed -i "s/.*BASE_URL.*/window.BASE_URL=\"$(echo $URL | sed -En 's/\//\\\//pg')\";/g" /var/www/index.html
 
 nginx -g "daemon off;"
