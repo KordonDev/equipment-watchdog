4308ea81a4d7c5426651c1b6b2e1a2a82b30f5c9
Arne Maier <Arne_Maier@gmx.de>
2023-12-22 08:57:09 +0100

    Update go packages

--- security/controller.go
+++ security/controller.go
@@ -1,42 +1,31 @@
 package security
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/duo-labs/webauthn.io/session"
-	"github.com/duo-labs/webauthn/protocol"
-	"github.com/duo-labs/webauthn/webauthn"
 	"github.com/gin-gonic/gin"
+	"github.com/go-webauthn/webauthn/protocol"
 	"github.com/kordondev/equipment-watchdog/models"
 	"github.com/kordondev/equipment-watchdog/url"
 )
 
 type Service interface {
-	startRegister(string) (*protocol.CredentialCreation, *webauthn.SessionData, error)
-	finishRegistration(string, webauthn.SessionData, *http.Request) (*models.User, error)
-	startLogin(string, *http.Request) (*protocol.CredentialAssertion, *webauthn.SessionData, error)
-	finishLogin(string, webauthn.SessionData, *http.Request) (*models.User, string, error)
+	startRegister(string) (*protocol.CredentialCreation, error)
+	finishRegistration(string, *http.Request) (*models.User, error)
+	startLogin(string, *http.Request) (*protocol.CredentialAssertion, error)
+	finishLogin(string, *http.Request) (*models.User, string, error)
 }
 
 type Controller struct {
-	service      Service
-	sessionStore *session.Store
-	domain       string
+	service Service
+	domain  string
 }
 
 func NewController(baseUrl *gin.RouterGroup, service Service, domain string) error {
-	sessionStore, err := session.NewStore()
-	if err != nil {
-		log.Fatal("Error creating sessionStore", err)
-		return fmt.Errorf("could not create session-store: %w", err)
-	}
 
 	ctrl := Controller{
-		service:      service,
-		sessionStore: sessionStore,
-		domain:       domain,
+		service: service,
+		domain:  domain,
 	}
 	baseUrl.GET("/register/:username", ctrl.startRegister)
 	baseUrl.POST("/register/:username", ctrl.finishRegistration)
@@ -50,17 +39,17 @@ func NewController(baseUrl *gin.RouterGroup, service Service, domain string) err
 func (ctrl Controller) startRegister(c *gin.Context) {
 	username := c.Param("username")
 
-	options, sessionData, err := ctrl.service.startRegister(username)
+	options, err := ctrl.service.startRegister(username)
 	if err != nil {
 		c.AbortWithError(http.StatusBadRequest, err)
 		return
 	}
 
-	err = ctrl.sessionStore.SaveWebauthnSession("registration", sessionData, c.Request, c.Writer)
+	/*err = ctrl.sessionStore.SaveWebauthnSession("registration", sessionData, c.Request, c.Writer)
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
-	}
+	}*/
 
 	c.JSON(http.StatusOK, options)
 }
@@ -68,13 +57,7 @@ func (ctrl Controller) startRegister(c *gin.Context) {
 func (ctrl Controller) finishRegistration(c *gin.Context) {
 	username := c.Param("username")
 
-	sessionData, err := ctrl.sessionStore.GetWebauthnSession("registration", c.Request)
-	if err != nil {
-		c.AbortWithError(http.StatusBadRequest, err)
-		return
-	}
-
-	user, err := ctrl.service.finishRegistration(username, sessionData, c.Request)
+	user, err := ctrl.service.finishRegistration(username, c.Request)
 	if err != nil {
 		c.AbortWithError(http.StatusBadRequest, err)
 		return
@@ -86,14 +69,8 @@ func (ctrl Controller) finishRegistration(c *gin.Context) {
 func (ctrl Controller) startLogin(c *gin.Context) {
 	username := c.Param("username")
 
-	options, sessionData, err := ctrl.service.startLogin(username, c.Request)
+	options, err := ctrl.service.startLogin(username, c.Request)
 
-	if err != nil {
-		c.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
-
-	err = ctrl.sessionStore.SaveWebauthnSession("authentication", sessionData, c.Request, c.Writer)
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
@@ -105,13 +82,7 @@ func (ctrl Controller) startLogin(c *gin.Context) {
 func (ctrl Controller) finishLogin(c *gin.Context) {
 	username := c.Param("username")
 
-	sessionData, err := ctrl.sessionStore.GetWebauthnSession("authentication", c.Request)
-	if err != nil {
-		c.AbortWithError(http.StatusBadRequest, err)
-		return
-	}
-
-	user, token, err := ctrl.service.finishLogin(username, sessionData, c.Request)
+	user, token, err := ctrl.service.finishLogin(username, c.Request)
 	if err != nil {
 		c.AbortWithError(http.StatusBadRequest, err)
 		return
