b9aab81e994e5a028c92e6da5a2c036329d97b27
Arne Maier <Arne_Maier@gmx.de>
2025-09-30 07:10:33 +0200

    Move notification to foreground

--- frontend/src/lib/components/Notification.svelte
+++ frontend/src/lib/components/Notification.svelte
@@ -2,7 +2,7 @@
 	import { notifications } from '$lib/services/notification.svelte';
 </script>
 
-<div class="fixed top-4 right-4 z-50 ">
+<div class="fixed top-4 right-4 z-5000 ">
 	{#each notifications as notification}
 		<div class="my-1 px-6 py-3 rounded shadow-lg bg-red-600 text-white font-semibold transition-all">
 			{notification.message}
